c27860ba5644e40020f66ce104e5788c91ef1380
Dustar Mao <mao.zili@live.cn>
2017-12-05 17:20:38 +0800

    throttled functions with different arguments are regard as different functions

--- src/async/throttle.ts
+++ src/async/throttle.ts
@@ -1,9 +1,20 @@
+type ArgumentsSerialize = (args: any[]) => string
+const defaultSerialize: ArgumentsSerialize = (args) => JSON.stringify(args)
 export function throttle<F extends (...args: any[]) => Promise<any>>(fn: F): F {
-  let prev: Promise<any> = null
-  return function() {
-    if (prev == null) {
-      prev = Promise.resolve(fn.apply(this, arguments))
+  const promises: {
+    [argToken: string]: Promise<any>
+  } = {}
+  return function(...args: any[]) {
+    const argToken = defaultSerialize(args)
+    if (typeof promises[argToken] !== "object") {
+      promises[argToken] = (async () => {
+        try {
+          return await fn.apply(this, arguments)
+        } finally {
+          delete promises[argToken]
+        }
+      })()
     }
-    return prev
+    return promises[argToken]
   } as F
 }
--- test/async/throttle.spec.ts
+++ test/async/throttle.spec.ts
@@ -3,20 +3,43 @@ import {delay} from "../../src/async/delay"
 import {expect} from "chai"
 
 describe("throttle", function() {
-  it("throttled function will not run while previous one not completed", function() {
-    const fn = (shouldRun: boolean) => delay(10, () => { expect(shouldRun).be.true })
+  it("throttled function will not run while previous one not completed", async function() {
+    let callCount = 0
+    const fn = () => {
+      callCount++
+      return delay(10)
+    }
     const throttled = throttle(fn)
-    const t1 = throttled(true)
-    const t2 = throttled(false)
-    return Promise.all([t1, t2])
+    const t1 = throttled()
+    const t2 = throttled()
+    await Promise.all([t1, t2])
+    expect(callCount).equals(1)
   })
 
   it("throttled function called while previous one not completed will be resolved with previous result", async function() {
-    const fn = (value: string) => delay(10, () => value)
+    let callCount = 0
+    const fn = () => {
+      const curCnt = ++callCount
+      return delay(10, () => curCnt)
+    }
     const throttled = throttle(fn)
-    const t1 = throttled("first")
-    const t2 = throttled("second")
+    const t1 = throttled()
+    const t2 = throttled()
     const [r1, r2] = await Promise.all([t1, t2])
-    expect(r1).equals(r2).equals("first")
+    expect(r1).equals(r2).equals(1)
+  })
+
+  it("throttled functions with different arguments are regard as different throttled functions", async function() {
+    let callCount = 0
+    const fn = (arg: any) => {
+      const curCnt = ++callCount
+      return delay(10, () => curCnt)
+    }
+    const throttled = throttle(fn)
+    const t1 = throttled('a')
+    const t2 = throttled('b')
+    const [r1, r2] = await Promise.all([t1, t2])
+    expect(r1).equals(1)
+    expect(r2).equals(2)
   })
 })
