4724fa460be809f6a1db910ff6a7571d5773b380
Dustar Mao <mao.zili@live.cn>
2017-12-05 12:22:11 +0800

    change delay argument sort

--- src/async/delay.ts
+++ src/async/delay.ts
@@ -1,7 +1,5 @@
-export function delay<T = void>(fn?: () => T|Promise<T>, timeout: number = 0) {
-  return timeout <= 0 ?
-    typeof fn === "function" ? Promise.resolve(fn()) : Promise.resolve() :
-    new Promise<T>((resolve, reject) => {
-      setTimeout(() => typeof fn === "function" ? resolve(fn()) : resolve(), timeout)
-    })
+export function delay<T = void>(timeout: number, fn?: () => T|Promise<T>) {
+  return new Promise<T>((resolve, reject) => {
+    setTimeout(() => typeof fn === "function" ? resolve(fn()) : resolve(), timeout)
+  })
 }
--- test/async/delay.spec.ts
+++ test/async/delay.spec.ts
@@ -5,22 +5,14 @@ describe("delay", function() {
 
   it("delayed function should run delayed", async function() {
     const beg = Date.now()
-    const timeout = 200
-    await delay(() => {
+    const timeout = 20
+    await delay(timeout, () => {
       expect(Date.now()).greaterThan(beg + timeout)
-    }, timeout)
-  })
-
-  it("no timeout will run immediately", async function() {
-    const beg = Date.now()
-    await delay(() => {
-      expect(Date.now()).equals(beg)
     })
   })
 
   it("no delay function will return Promise<void>", async function() {
-    await delay().then(data => expect(data).undefined)
-    await delay(undefined, 100).then(data => expect(data).undefined)
+    await delay(10).then(data => expect(data).undefined)
   })
 
 })
